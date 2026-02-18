a84e97dbc96441bad82f8d825320cd20ab6503a1
sorin-grigorescu <sorin.mihai.grigorescu@gmail.com>
2026-02-18 17:56:06 +0200

    Updated waypoints planner.

--- src/cyc_toolkit/src/plan/CWaypointsPlanner.cpp
+++ src/cyc_toolkit/src/plan/CWaypointsPlanner.cpp
@@ -2,6 +2,7 @@
 // Author: Sorin Mihai Grigorescu
 
 #include "CWaypointsPlanner.h"
+#include "math/CGeometry.h"
 #include <iostream>
 #include <sstream>
 #include <algorithm>
@@ -16,6 +17,11 @@ CWaypointsPlanner::CWaypointsPlanner(std::string _waypoints_file)
 
 CWaypointsPlanner::~CWaypointsPlanner()
 {}
+
+float CWaypointsPlanner::dist(float x1, float y1, float x2, float y2)
+{
+    return sqrtf(powf(x2 - x1, 2.f) + powf(y2 - y1, 2.f));
+}
 
 bool CWaypointsPlanner::loadWaypoints(const std::string& _waypoints_file)
 {
@@ -144,12 +150,12 @@ bool CWaypointsPlanner::getMarker(const CyC_INT _marker_id, CycLandmark& _landma
     }
 }
 
-bool CWaypointsPlanner::getLandmark(const CyC_INT _marker_id, CycLandmark& _waypoints)
+bool CWaypointsPlanner::getLandmark(const CyC_INT _marker_id, CycLandmark& _landmark)
 {
     if (m_Landmarks.find(_marker_id) == m_Landmarks.end())
         return false;
 
-    _waypoints = m_Landmarks[_marker_id];
+    _landmark = m_Landmarks[_marker_id];
 
     return true;
 }
@@ -215,4 +221,198 @@ void CWaypointsPlanner::str2waypoints(std::string _str_waypoints, std::vector<Ei
 
     for (auto it = tmp_waypoints.begin(); it != tmp_waypoints.end(); ++it)
         _waypoints.emplace_back(*it);
+}
+
+bool CWaypointsPlanner::addLandmark(const int& _id, const CPose& _pose, const std::vector<Eigen::VectorXf>& _waypoints)
+{
+    if (m_Landmarks.count(_id))
+        return false;
+
+    CycLandmark landmark;
+    for (const auto& waypt : _waypoints)
+    {
+        if (waypt.size() < 4)
+            return false;
+        landmark.waypoints.emplace_back(waypt.head<4>());
+    }
+    m_Landmarks.emplace(_id, landmark);
+
+    return true;
+}
+
+void CWaypointsPlanner::landmark2mission(const CycLandmark& _landmark, std::vector<Eigen::Vector4f>& _mission_pts)
+{
+    std::vector<Eigen::Vector2f> waypoints;
+    for (const auto& w : _landmark.waypoints)
+        waypoints.emplace_back(w.x(), w.y());
+    waypoints.emplace_back(_landmark.pose.translation_3x1().x(), _landmark.pose.translation_3x1().y());
+
+    waypoints2mission(waypoints, _mission_pts);
+}
+
+void CWaypointsPlanner::waypoints2mission(const std::vector<Eigen::Vector2f>& waypoints, std::vector<Eigen::Vector4f>& mission_pts)
+{
+    mission_pts.clear();
+    const bool interpolate_points = true;
+    const float velocity = 1.F;  // Reference velocity
+    float yaw = 0.F;       // Default yaw
+
+    for (const auto& w : waypoints)
+    {
+        float x = w.x(), y = w.y();
+
+        if (mission_pts.size() > 0)
+        {
+            const auto prev_x = mission_pts.back()[0];
+            const auto prev_y = mission_pts.back()[1];
+
+            // Estimate yaw angle for each state
+            yaw = atan2f(y - prev_y, x - prev_x);
+
+            // Interpolate points in between measurements
+            if (interpolate_points)
+            {
+                const auto prev_angle = mission_pts.back()[3];
+                const Eigen::Vector4f begin{ prev_x, prev_y, velocity, prev_angle };
+                const Eigen::Vector4f end{ x, y, velocity, yaw };
+                interpolate_between_points(begin, end, mission_pts);
+            }
+        }
+
+        mission_pts.emplace_back(x, y, velocity, yaw);
+    }
+}
+
+bool CWaypointsPlanner::waypoints2mission(csv::reader& csv_reader, std::vector<Eigen::Vector4f>& _out_ref_path_pts)
+{
+    std::vector<Eigen::Vector2f> pts;
+
+    csv_reader.select_cols("x", "y", "waypoints");
+    float x, y;
+    std::string waypoints;
+    float x_wpt, y_wpt;
+    std::string delimiter_outside = "[";
+    std::string delimiter_inside = ";";
+    while (csv_reader.read_row(x, y, waypoints))
+    {
+        // Insert landmark to the reference set points vector
+        pts.emplace_back(x, y);
+
+        // Remove first two "["
+        waypoints = waypoints.substr(2, waypoints.size() - 1);
+
+        // Replace trailing "]" with delimiter "["
+        waypoints[waypoints.size() - 1] = '[';
+
+        // Split waypoints
+        unsigned long long pos = 0;
+        std::string waypoint;
+        while ((pos = waypoints.find(delimiter_outside)) != std::string::npos)
+        {
+            waypoint = waypoints.substr(0, pos);
+
+            // Replace trailing "]" with delimiter ";"
+            waypoint[waypoint.size() - 1] = ';';
+
+            unsigned long long pos2 = 0;
+            std::string wpt_element;
+            CyC_INT i = 0;
+            while ((pos2 = waypoint.find(delimiter_inside)) != std::string::npos)
+            {
+                wpt_element = waypoint.substr(0, pos2);
+
+                if (i == 0)
+                {
+                    x_wpt = std::stof(wpt_element);
+                }
+                if (i == 1)
+                {
+                    y_wpt = std::stof(wpt_element);
+                }
+                i++;
+                waypoint.erase(0, pos2 + delimiter_inside.length());
+            }
+
+            pts.emplace_back(x_wpt, y_wpt);
+
+            waypoints.erase(0, pos + delimiter_outside.length());
+        }
+    }
+
+    waypoints2mission(pts, _out_ref_path_pts);
+
+    return true;
+}
+
+void CWaypointsPlanner::interpolate_between_points(const Eigen::Vector4f& begin, const Eigen::Vector4f& end, std::vector<Eigen::Vector4f>& _in_out_ref_path_pts)
+{
+    const float sampling_distance = 0.1f;  // If points will be interpolated, use this distance
+
+    const auto prev_x = begin[0];
+    const auto prev_y = begin[1];
+
+    const auto x = end[0];
+    const auto y = end[1];
+    const auto v = end[2];
+    const auto yaw = end[3];
+
+    if (!_in_out_ref_path_pts.empty())
+    {
+#ifdef USE_OLD_METHOD // Uses first degree polynomials. Can't interpolate if slope is infinite
+        const float m = (y - prev_y) / (x - prev_x);
+        if (abs(x - prev_x) < FLT_EPSILON)
+            return;
+
+        const float b = y - m * x;
+        const float d = dist(x, y, prev_x, prev_y);
+        if (d > sampling_distance)
+        {
+            const CyC_INT num_interpolate = (CyC_INT)(d / sampling_distance);
+            for (CyC_INT interp_idx = 0; interp_idx < num_interpolate; ++interp_idx)
+            {
+                const float new_x = prev_x + interp_idx * ((x - prev_x) / num_interpolate);
+                const float new_y = m * new_x + b;
+                const float new_yaw = atan2f(new_y - prev_y, new_x - prev_x);
+                _in_out_ref_path_pts.emplace_back(new_x, new_y, v, new_yaw);
+            }
+        }
+#else
+        const float d = dist(x, y, prev_x, prev_y);
+        const CyC_INT num_interpolate = (CyC_INT)ceil(d / sampling_distance);
+        if (num_interpolate > 0)
+        {
+            const float dx = (x - prev_x) / num_interpolate;
+            const float dy = (y - prev_y) / num_interpolate;
+            float new_x = prev_x;
+            float new_y = prev_y;
+            for (CyC_INT interp_idx = 0; interp_idx < num_interpolate; ++interp_idx)
+            {
+                new_x += dx;
+                new_y += dy;
+
+                const float new_yaw = atan2f(new_y - prev_y, new_x - prev_x);
+                _in_out_ref_path_pts.emplace_back(new_x, new_y, v, new_yaw);
+            }
+        }
+#endif
+    }
+}
+
+CycLandmark CWaypointsPlanner::getClosestTraj(const CycLandmarks& _landmarks, const CPose& _dst)
+{
+    float fMinEd = std::numeric_limits<float>::max();
+    CycLandmark ref_traj;
+    Eigen::Vector2f destination = _dst.translation_3x1().head<2>();
+    for (const auto& traj : _landmarks)
+    {
+        Eigen::Vector2f landmark = traj.second.pose.translation_3x1().head<2>();
+
+        float fEd = CGeometry::euclidean_dist(destination, landmark);
+        if (fEd < fMinEd)
+        {
+            ref_traj = traj.second;
+            fMinEd = fEd;
+        }
+    }
+    return ref_traj;
 }
--- src/cyc_toolkit/src/plan/CWaypointsPlanner.h
+++ src/cyc_toolkit/src/plan/CWaypointsPlanner.h
@@ -19,14 +19,24 @@ public:
     bool        loadWaypoints(const CycLandmarks& refs);
     static bool saveWaypoints(CycLandmarks& _landmarks, const std::string& _waypoints_file);
 
+    bool        addLandmark(const int& _id, const CPose& _pose, const std::vector<Eigen::VectorXf>& _waypoints);
+
     const std::unordered_map<CyC_INT, CycLandmark>& getLandmarks() const { return m_Landmarks; };
-    bool        getLandmark(const CyC_INT _marker_id, CycLandmark& _waypoints);
+    bool        getLandmark(const CyC_INT _marker_id, CycLandmark& _landmark);
     bool        getMarker(const CyC_INT _marker_id, CycLandmark& _landmark);
     bool        getWaypoints(const CyC_INT _marker_id, std::vector<Eigen::Vector4f>& _waypoints);
     CyC_UINT    getNumLandmarks();
 
+    static void landmark2mission(const CycLandmark& _landmark, std::vector<Eigen::Vector4f>& _mission_pts);
+    static void waypoints2mission(const std::vector<Eigen::Vector2f>& waypoints, std::vector<Eigen::Vector4f>& mission_pts);
+    static bool waypoints2mission(csv::reader& csv_reader, std::vector<Eigen::Vector4f>& _out_ref_path_pts);
+
+    static void interpolate_between_points(const Eigen::Vector4f& begin, const Eigen::Vector4f& end, std::vector<Eigen::Vector4f>& _in_out_ref_path_pts);
+    static CycLandmark getClosestTraj(const CycLandmarks& _landmarks, const CPose& _dst);
+
 private:
     void str2waypoints(std::string _str_waypoints, std::vector<Eigen::Vector4f>& _waypoints);
+    static float dist(float x1, float y1, float x2, float y2);
 
 private:
     CycLandmarks m_Landmarks;
