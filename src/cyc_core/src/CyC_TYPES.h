5cbdb412e5d3e72faed703e4037d0c191a5d3e67
sorin-grigorescu <sorin.mihai.grigorescu@gmail.com>
2026-09-25 06:00:36 +0300

    fixed spelling satellites

--- src/cyc_core/src/CyC_TYPES.h
+++ src/cyc_core/src/CyC_TYPES.h
@@ -617,7 +617,7 @@ struct CycGps
     friend auto operator<<(std::ostream& _os, CycGps const& _gps) -> std::ostream&
     {
         const auto prec = _os.precision(7);
-        _os << _gps.timestamp << ":\t" << _gps.latitude << "\t" << _gps.longitude << "\t" << _gps.altitude << "\t(sats: " << _gps.num_satelites << ", fix: " << _gps.fix_type << ")";
+        _os << _gps.timestamp << ":\t" << _gps.latitude << "\t" << _gps.longitude << "\t" << _gps.altitude << "\t(sats: " << _gps.num_satellites << ", fix: " << _gps.fix_type << ")";
         _os.precision(prec);
         return _os;
     }
